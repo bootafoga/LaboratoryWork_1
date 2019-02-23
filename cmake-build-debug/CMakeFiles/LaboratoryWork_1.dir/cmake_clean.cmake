84b440de5680376ed3a7c90c4b6ffadc8583cb4d
Мария <m.juice@icloud.com>
2019-02-23 18:03:29 +0300

    Smth on the Cl

---
+++ cmake-build-debug/CMakeFiles/LaboratoryWork_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LaboratoryWork_1.dir/main.cpp.obj"
+  "CMakeFiles/LaboratoryWork_1.dir/Tests/UnitTests.cpp.obj"
+  "LaboratoryWork_1.pdb"
+  "LaboratoryWork_1.exe"
+  "LaboratoryWork_1.exe.manifest"
+  "libLaboratoryWork_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LaboratoryWork_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
